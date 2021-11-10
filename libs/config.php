bc5fed7d03cf05a70a774c726f65229af3aac3fd
Vinicius Silva <code@seelv.dev>
2021-11-10 16:28:42 -0300

    Update config.php

--- libs/config.php
+++ libs/config.php
@@ -3,8 +3,8 @@
 
     define('DB_HOST','localhost');
     define('DB_USER','root');
-    define('DB_PASS','a');
-    define('DB_SCHEMA','ultradevdb');
+    define('DB_PASS','root');
+    define('DB_SCHEMA','');
 
     define('PTR_LINK',$_SERVER['SERVER_NAME']);
     define('DEVMODE',true);
